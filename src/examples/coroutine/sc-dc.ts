9c7ba46091b99bd5c65572f61137753ec40e4035
Charles Lowell <cowboyd@frontside.com>
2024-04-18 22:42:55 -0500

    Put a delimiter around the recursion

--- src/examples/coroutine/sc-dc.ts
+++ src/examples/coroutine/sc-dc.ts
@@ -1,7 +1,7 @@
 import { probeMemory } from "../../benchmark/probe";
 import { Options, RunContext } from "../../benchmark/types";
 import { breadth } from "./_common";
-import { Operation, evaluate, shift } from "../../implementation/sc-dc/index";
+import { Operation, evaluate, reset, shift } from "../../implementation/sc-dc/index";
 
 export function setup(options: Options): void {}
 
@@ -9,24 +9,28 @@ export async function run({ depth }: RunContext): Promise<void> {
   return new Promise((resolve, reject) => {
     evaluate(function*() {
       try {
-	yield* recurse(depth);
-	resolve();
+        yield* reset(function*() {
+          yield* recurse(depth);
+        });
+        resolve();
       } catch (error) {
-	reject(error);
+        reject(error);
       }
     })
   })
 }
 
 function* recurse(depth: number): Operation<void> {
-    if (depth > 1) {
-      yield* recurse(depth - 1);
-    } else {
+  if (depth > 1) {
+    yield* recurse(depth - 1);
+  } else {
+    yield* shift(function*() {
       for (let i = 0; i < breadth; i++) {
         yield* shift(function*(k, reenter) {
-	  Promise.resolve().then(() => reenter(k, void 0))
-	});
+          Promise.resolve().then(() => reenter(k, void 0))
+        });
       }
       probeMemory("bottom");
-    }
+    });
+  }
 }
